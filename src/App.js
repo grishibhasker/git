1dc85fd78e6d42aac742800d5f71001deaeab8d0
rishi <rishibhaskarg@gmail.com>
2023-05-12 11:59:43 +0530

    RRR movie

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>RRR Movie</h1>
+  <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfTh8E7tZbi-Z4YzNZIQ-lzdPFUQNYXxjASvYoQeVsTQ&usqp=CAU&ec=48665701'></img>
+  <h3>  main team</h3>
+  <h3>  RAM CHARAN TEJ
+  </h3>
+  <h3> NTR
+  </h3>
+  <h3>  SSR
+  </h3>
+  <h3>  DVV
+  </h3>
+    </div>
+  );
+}
+
+export default App;
